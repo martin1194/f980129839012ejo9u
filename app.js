2a73f1de86ba75b470b137b8036f6fa30de5de06
martin1194 <martinc1194@gmail.com>
2018-12-30 05:21:07 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const config = require("./config.json");
+const bot = new Discord.Client();
+
+bot.on('ready',() => {
+  console.log(`成功登入 ${bot.user.tag}`);
+});
+
+bot.on('message', msg => {
+  //變量(variables)
+  //var sender = message.author; // The person who sent the message
+  //var msg = message.content.toUpperCase(); // Takes the message, and makes it all uppercase
+  var prefix = '/' // The text before commands, you can set this to what ever you want
+
+  if(msg.content === '嗨'){
+    msg.reply('嗨');
+  }
+
+  if (msg.content === config.prefix + 'help'){
+    msg.reply({embed:{
+      title: "想得到幫助?" ,
+      description: "暫時沒有資料^^" ,
+      color: 0xFF4E28
+    }})
+  }
+});
+
+bot.on('guildMemberAdd', function(member)
+{
+  member.send("歡迎來到這白癡熊的伺服器XD");
+  //member.guild.channels.get(config.lobby).send('**' + member.user.username + '**加入到頻道');
+})  ;
+bot.on('guildMemberRemove', member => {
+
+  //member.guild.channels.get(config.lobby).send('**' + member.user.username + '**離開到頻道');
+})
+//bot.login(config.token);
+client.login(process.env.BOT_TOKEN)
