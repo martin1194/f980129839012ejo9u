f5faae48a7262492e025ff80cd8f83c0eed0cf3c
martin1194 <martinc1194@gmail.com>
2018-12-30 14:13:58 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 
 
 const Discord = require('discord.js');
-const config = require("./config.json");
+//const config = require("./config.json");
 const bot = new Discord.Client();
 
 bot.on('ready',() => {
