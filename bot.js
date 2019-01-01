c4001415ed9b26594c6ef65ed0d3b47c99c79094
martin1194 <martinc1194@gmail.com>
2019-01-01 17:01:08 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('ready',() => {
     })
 
 //  console.log(`成功登入 ${client.user.tag}`);
-  client.user.setStatus('Online') // Online/idle/invisible/dnd
+  client.user.setStatus('Online'); // Online/idle/invisible/dnd
 //  client.user.setGame('say /noice for noice') //
   client.user.setActivity("毛毛", { type: "PLAYING"})//WATCHING;PLAYING
 
