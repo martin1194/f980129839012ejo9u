2ad9a9899edd2488a7451e508bd746952796a4b6
martin1194 <martinc1194@gmail.com>
2019-01-04 00:42:44 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('ready',() => {
 //  console.log(`成功登入 ${client.user.tag}`);
   client.user.setStatus('Online'); // Online/idle/invisible/dnd
 //  client.user.setGame('say /noice for noice') //
-  client.user.setActivity("STEAM", { type: "PLAYING"})//WATCHING;PLAYING
+  client.user.setActivity("劍靈", { type: "PLAYING"})//WATCHING;PLAYING
 
 });
 //變量(variables)
