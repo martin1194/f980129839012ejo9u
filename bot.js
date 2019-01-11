4d82961f7ddbae37db85a69589745770b19617aa
martin1194 <martinc1194@gmail.com>
2019-01-12 04:31:18 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,17 +60,17 @@ client.on('message', msg => {
     }});
     msg.reply({embed:{
       title: "未來上課日" ,
-      description: "輸入「!001」可查看未來時間安排" ,
+      description: "輸入「!001」可查看未來上課日" ,
       color: 0xFF4E28
     }})
     msg.reply({embed:{
       title: "未來加班日" ,
-      description: "輸入「!002」可查看未來時間安排" ,
+      description: "輸入「!002」可查看未來加班日" ,
       color: 0xFF4E28
     }})
     msg.reply({embed:{
       title: "未來休息日" ,
-      description: "輸入「!003」可查看未來時間安排" ,
+      description: "輸入「!003」可查看未來休息日" ,
       color: 0xFF4E28
     }})
   }
